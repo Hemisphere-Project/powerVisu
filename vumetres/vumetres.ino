e78296b99f5821db0d93d34c34fe384d605b1d7a
Jeremie <JeremieForge@users.noreply.github.com>
2018-11-29 15:20:41 +0100

    oo

--- vumetres/vumetres.ino
+++ vumetres/vumetres.ino
@@ -24,21 +24,21 @@ Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(NUMPIXELS_STRIP2, OUTPIN2, NEO_GRB 
 Adafruit_NeoPixel strip3 = Adafruit_NeoPixel(NUMPIXELS_STRIP3, OUTPIN3, NEO_GRB + NEO_KHZ800);
 
 // BRIGHTNESS
-int lightMax = 150;
+int lightMax = 20;
 
 ///////////////////////// INPUTS /////////////////////////
 // INPUT 1
-#define INPIN1 A0
-int data1Min = 10;
-int data1Max = 600;
+#define INPIN1 A0 // 0-0,2V --
+int data1Min = 0;
+int data1Max = 5;
 // INPUT 2
-#define INPIN2 A1
+#define INPIN2 A1 //
 int data2Min = 10;
-int data2Max = 50;
+int data2Max = 614;
 // INPUT 3
-#define INPIN3 A2
+#define INPIN3 A2 // PONT DIVISEUR 0-3V
 int data3Min = 10;
-int data3Max = 50;
+int data3Max = 614;
 
 /////////////////////// SMOOTHING ///////////////////////
 // 1
@@ -85,26 +85,37 @@ void setup() {
 ///////////////////////// LOOP /////////////////////////
 
 void loop() {
-  // randomValue=randomValue+sens;
-  // if ((randomValue>100)||(randomValue<0)){ sens = -1*sens; }
+
+  // randomValue=randomValue+sens*2;
+  // if ((randomValue>600)||(randomValue<0)){ sens = -1*sens; }
 
   // int in1 = analogRead(INPIN1);
   // int in2 = analogRead(INPIN2);
   // int in3 = analogRead(INPIN3);
 
-  // Serial.print(analogRead(INPIN1));
-  // Serial.print(" ");
-  // Serial.print(analogRead(INPIN2));
-  // Serial.print(" ");
-  // Serial.println(analogRead(INPIN3));
-
   int in1Smoothed = in1Smoothing( analogRead(INPIN1) );
   int in2Smoothed = in2Smoothing( analogRead(INPIN2) );
   int in3Smoothed = in3Smoothing( analogRead(INPIN3) );
 
+  // Serial.print(analogRead(INPIN1));
+  // Serial.print(" ");
+  // Serial.print(analogRead(INPIN2));
+  // Serial.print(" ");
+  // Serial.print(analogRead(INPIN3));
+  // Serial.print(" ----> ");
+  // Serial.print(in1Smoothed);
+  // Serial.print(" ");
+  // Serial.print(in2Smoothed);
+  // Serial.print(" ");
+  // Serial.println(in3Smoothed);
+
   showValueStrip1("allG2R", in1Smoothed);
   showValueStrip2("allG2R", in2Smoothed);
   showValueStrip3("allG2R", in3Smoothed);
+
+  // showValueStrip1("allG2R", randomValue);
+  // showValueStrip2("allG2R", randomValue);
+  // showValueStrip3("allG2R", randomValue);
 
   // Update
   strip1.show();
