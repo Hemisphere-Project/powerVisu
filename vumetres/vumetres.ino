390878e61fe6f2ace3b85acd8e98343028dc93f8
Jeremie <JeremieForge@users.noreply.github.com>
2018-11-30 18:57:33 +0100

    ok

--- vumetres/vumetres.ino
+++ vumetres/vumetres.ino
@@ -24,7 +24,7 @@ Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(NUMPIXELS_STRIP1, OUTPIN1, NEO_GRB 
 Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(NUMPIXELS_STRIP2, OUTPIN2, NEO_GRB + NEO_KHZ800);
 // STRIP 3
 #define OUTPIN3            7
-#define NUMPIXELS_STRIP3   30
+#define NUMPIXELS_STRIP3   87
 Adafruit_NeoPixel strip3 = Adafruit_NeoPixel(NUMPIXELS_STRIP3, OUTPIN3, NEO_GRB + NEO_KHZ800);
 
 
@@ -33,16 +33,19 @@ int lightMax = 150;
 
 ///////////////////////// INPUTS /////////////////////////
 // INPUT 1
+// Courant produit par la génératrice
 #define INPIN1 A0 // 0-0,2V --
-long data1Min = 0;
-long data1Max = 614;//5
+long data1Min = 510;
+long data1Max = 540;
 // INPUT 2
+// Courant consommé
 #define INPIN2 A1 //
-long data2Min = 10;
-long data2Max = 614;
+long data2Min = 500;
+long data2Max = 530;
 // INPUT 3
+// V Batterie
 #define INPIN3 A2 // PONT DIVISEUR 0-3V
-long data3Min = 10;
+long data3Min = 0;
 long data3Max = 614;
 
 /////////////////////// SMOOTHING ///////////////////////
@@ -84,6 +87,10 @@ void setup() {
   for (int i = 0; i < numAvg2; i++) { in2Raw[i] = 0; }
   for (int i = 0; i < numAvg3; i++) { in3Raw[i] = 0; }
 
+  showValueStrip1("allG2R", 0);
+  showValueStrip2("allG2R", 0);
+  showValueStrip3("allG2R", 0);
+
 }
 
 
@@ -94,33 +101,21 @@ void loop() {
   // randomValue=randomValue+sens*6;
   // if ((randomValue>600)||(randomValue<0)){ sens = -1*sens; }
 
-  // int in1 = analogRead(INPIN1);
-  // int in2 = analogRead(INPIN2);
-  // int in3 = analogRead(INPIN3);
-
   int in1Smoothed = in1Smoothing( analogRead(INPIN1) );
   int in2Smoothed = in2Smoothing( analogRead(INPIN2) );
   int in3Smoothed = in3Smoothing( analogRead(INPIN3) );
 
-  // Serial.print(analogRead(INPIN1));
-  // Serial.print(" ");
-  // Serial.print(analogRead(INPIN2));
-  // Serial.print(" ");
-  // Serial.print(analogRead(INPIN3));
-  // Serial.print(" ----> ");
-  // Serial.print(in1Smoothed);
-  // Serial.print(" ");
-  // Serial.print(in2Smoothed);
-  // Serial.print(" ");
-  // Serial.println(in3Smoothed);
+  // LOG
+  Serial.print(" 1: i prod ");
+  Serial.print(in1Smoothed);
+  Serial.print(" 2: i conso ");
+  Serial.print(in2Smoothed);
+  Serial.print(" 3: V batt ");
+  Serial.println(in3Smoothed);
 
   showValueStrip1("allG2R", in1Smoothed);
   showValueStrip2("allG2R", in2Smoothed);
   showValueStrip3("allG2R", in3Smoothed);
-
-  // showValueStrip1("allG2R", randomValue);
-  // showValueStrip2("allG2R", randomValue);
-  // showValueStrip3("allG2R", randomValue);
 
   // Update
   strip1.show();
